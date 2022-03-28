0bec60e33a57cc2beadb8b6b7db6f8e8af881d1c
Brandon Helt <brandon.helt@yahoo.com>
2022-03-27 20:09:14 -0700

    Completed exercises

--- index.js
+++ index.js
@@ -1,37 +1,88 @@
-function forEach() {
-
+function forEach(array, cb) {
+  for (let i = 0; i < array.length; i++) {
+    cb(array[i], i, array);
+  }
 }
 
-function map() {
-
+function map(array, cb) {
+  const newArray = [];
+  for (element of array) {
+    newArray.push(cb(element));
+  }
+  return newArray;
 }
 
-function filter() {
-
+function filter(array, cb) {
+  const newArray = [];
+  for (element of array) {
+    if (cb(element) === true) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 }
 
-function every() {
-
+function every(array, cb) {
+  let test = true;
+  for (element of array) {
+    if (cb(element) === false)
+      test = false;
+  }
+  return test;
 }
 
-function some() {
-
+function some(array, cb) {
+  let test = false;
+  for (element of array) {
+    if (cb(element) === true)
+      test = true;
+  }
+  return test;
 }
 
-function majority() {
-
+function majority(array, cb) {
+  let count = 0;
+  for (element of array) {
+    if (cb(element) === true)
+      count++;
+  }
+  let test = false;
+  if (count > (array.length / 2))
+    test = true;
+  return test;
 }
 
-function once() {
-
+function once(cb) {
+  let called = false;
+  let result;
+  return function() {
+    if (!called) {
+      result = cb.apply(this, arguments);
+      called = true;
+    }
+    return result;
+  }
+  
 }
 
-function groupBy() {
-
+function groupBy(array, cb) {
+  const myObject = {};
+  for (element of array) {
+    let key = cb(element);
+    if (key in myObject)
+      myObject[key].push(element);
+    else
+      myObject[key] = [element];
+  }
+  return myObject;
 }
 
-function arrayToObject() {
-
+function arrayToObject(array, cb) {
+  const myObject = {};
+  for (element of array) {
+    myObject[element] = cb(element);
+  }
+  return myObject;
 }
 
 module.exports = { 
